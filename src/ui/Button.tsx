9064781e1d18625741d75258aa099de2a4650454
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-11-15 19:22:55 +0300

    fix btn size

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,6 +1,6 @@
 export function Button() {
     return (
-        <button className="bg-button text-button text-sm px-5 py-2 mt-4 rounded-xl hover:opacity-80 transition-opacity">
+        <button className="bg-button w-full text-button text-sm px-5 py-2 mt-4 rounded-xl hover:opacity-80 transition-opacity">
             Начать просмотр
         </button>
     );
